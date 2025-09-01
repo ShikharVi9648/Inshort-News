309d48080ff7fff96a18228da38188a043186f91
ShikharVi9648 <shikharvish9648@gmail.com>
2025-09-01 09:45:43 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://newsdata.io/api/1/latest?&q=";
+const apiUrl = "https://newsdata.io/api/1/latest?q=";
 const apiKey = "pub_be613c8f57d6470ab2661482ecd0ae97";
 
 // const targetedCities = [
@@ -18,7 +18,7 @@ const apiKey = "pub_be613c8f57d6470ab2661482ecd0ae97";
 
 // --- DOM ELEMENTS ---
 const sidebar = document.querySelector(".sidebar");
-const sidebarMenu = document.querySelector(".sidebar-menu"); // The UL element for menu items
+const sidebarMenu = document.querySelector(".sidebar-menu");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const closeMenu = document.querySelector(".close");
 const newsCards = document.querySelectorAll(".main-card");
@@ -27,6 +27,7 @@ const loadMoreContainer = document.querySelector(".load-more");
 
 const langEnBtn = document.querySelector(".language-selection .english");
 const langHiBtn = document.querySelector(".language-selection .hindi");
+const langMarathiBtn = document.querySelector(".language-selection .marathi");
 
 // --- STATE MANAGEMENT ---
 let currentQuery = "";
@@ -193,6 +194,7 @@ if (langHiBtn) {
   langHiBtn.addEventListener("click", (e) => {
     e.preventDefault();
     langEnBtn.classList.remove("active");
+    langMarathiBtn.classList.remove("active");
     langHiBtn.classList.add("active");
 
     const queryForHindi =
@@ -200,4 +202,57 @@ if (langHiBtn) {
 
     fetchNews(queryForHindi, "hi");
   });
+}
+if (langMarathiBtn) {
+  langMarathiBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    langEnBtn.classList.remove("active");
+    langHiBtn.classList.remove("active");
+    langMarathiBtn.classList.add("active");
+
+    const queryForMarathi =
+      currentQuery.toLowerCase() === "india" ? "भारत" : currentQuery;
+
+    fetchNews(queryForMarathi, "mr");
+  });
+}
+
+//for voice button
+
+async function readText() {
+  const text = document.querySelector(".clamped-text").textContent.trim();
+
+  let lang = "en-US";
+
+  // Detect Hindi or Marathi
+  if (/[अ-ह]/.test(text)) {
+    if (/माझ्या|आपले|स्वागत/.test(text)) {
+      lang = "mr-IN";
+    } else {
+      lang = "hi-IN";
+    }
+  }
+
+  const response = await fetch(
+    "https://texttospeech.googleapis.com/v1/text:synthesize?key=AIzaSyCEW5pL4CQ3bww1KuR7wPTLxw31iIC9TSY",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: { text: text },
+        voice: { languageCode: lang, ssmlGender: "FEMALE" },
+        audioConfig: { audioEncoding: "MP3" },
+      }),
+    }
+  );
+
+  const data = await response.json();
+
+  if (data.audioContent) {
+    // Play the audio
+    let audio = new Audio("data:audio/mp3;base64," + data.audioContent);
+    audio.play();
+  } else {
+    console.error("Audio content not returned:", data);
+  }
 }
